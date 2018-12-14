eca386551b3a1c7d3627ee369c78bbc3b21167bf
weisd <weishidavip@163.com>
2018-12-14 11:48:15 +0800

    update readme

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,2 @@
-# ./bin/swagger
-./bin/swagger -deriver sql -dsn "mysql:web-kit:webkit@tcp(localhost)/web-kit?charset=utf8mb4&parseTime=True&loc=Local"
+./bin/swagger
+# ./bin/swagger -deriver sql -dsn "mysql:web-kit:webkit@tcp(localhost)/web-kit?charset=utf8mb4&parseTime=True&loc=Local"
